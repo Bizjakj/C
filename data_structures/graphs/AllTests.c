e44193d5c331e80a591f5ab55f33286723181ccc
Benjamin Warschauer <warschab@oregonstate.edu>
2019-03-05 23:33:49 -0800

    graph tests

---
+++ data_structures/graphs/AllTests.c
@@ -0,0 +1,23 @@
+ #include "CuTest.h"
+    
+    CuSuite* StrUtilGetSuite();
+    
+    void RunAllTests(void) {
+        CuString *output = CuStringNew();
+        CuSuite* suite = CuSuiteNew();
+        
+        //There are duplicate functions so bot hfiles can't be tested together, 
+        //to test Dijkstra's uncoment the first line and comment the second. 
+
+        //CuSuiteAddSuite(suite, DijkstraGetSuite());
+        CuSuiteAddSuite(suite, BellmanGetSuite());
+    
+        CuSuiteRun(suite);
+        CuSuiteSummary(suite, output);
+        CuSuiteDetails(suite, output);
+        printf("%s\n", output->buffer);
+    }
+    
+    int main(void) {
+        RunAllTests();
+    }
