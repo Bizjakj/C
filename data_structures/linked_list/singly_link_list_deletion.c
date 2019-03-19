691cc67b15df13ab339ecc1c7b21b03fd8670e29
bitterplant <bostelmf@oregonstate.edu>
2019-03-18 18:01:18 -0700

    refactored linked lists

--- data_structures/linked_list/singly_link_list_deletion.c
+++ data_structures/linked_list/singly_link_list_deletion.c
@@ -3,13 +3,18 @@
   function can be modified according to the data type, easily.
   deleteNode deletes a node when passed with a key of the node.
 */
+
 #include<stdio.h>
+#include<stdlib.h>
 #include<assert.h>
+
 struct node
-{int info;
+{
+ int info;
  struct node *link;
 };
 struct node *start=NULL;
+
 ///////////////////////////////////////////////////////////
 struct node * createnode()//function to create node
 {
@@ -22,12 +27,10 @@ void insert( int a)//function to insert at first location
 {
   struct node *p;
   p=createnode();
-  /*printf("\nenter the number to insert");
-  scanf("%d",&p->info);
-  */
   
   p->info = a;
   p->link=NULL;
+  
   if(start==NULL)
   {
       start=p;
@@ -39,7 +42,7 @@ void insert( int a)//function to insert at first location
   }
 }
 ///////////////////////////////////////////////////////////
-void deleteion()//function to delete from first position
+void deletion()//function to delete from first position
 {
     struct node *t;
     if(start==NULL)
@@ -71,98 +74,15 @@ void viewlist()//function to display values
         }
     }
 }
-//////////////////////////////////////////////////////////////////////
-
-/*int main()
-{
-    int n;
-    while(1)
-    {
-        printf("\n1.add value at first location");
-        printf("\n2.delete value from first location");
-        printf("\n3.view value");
-        printf("\nenter your choice");
-        scanf("%d",&n);
-        switch(n)
-        {
-        case 1:
-            insert();
-            break;
-        case 2:
-            deleteion();
-            break;
-        case 3:
-            viewlist();
-            break;
-        default:
-            printf("\ninvalid choice");
-        }
-    }
-  return(0);
-}
-
 
 int main(){
-  start = createnode();
-  struct node* og = start;
-  insert();
-  assert(og != start);
-  insert();
-  printf("List:\n");
-  viewlist();
-  printf("\n");
-  deleteion();
-  deleteion();
-  assert(og == start);
-  deleteion();
-  printf("List:\n");
-  viewlist();
-}
-
-*/
-
-int main(){
-	int a = 1;
-	int b = 3; 
-	int c = 9;
-	int d = 0;
-	
-	insert(a);
+	insert(12);
+	insert(2);
+	insert(7);
 	
 	viewlist();
-	printf("\n");
 	
-	insert(b);
-	
-	viewlist();
-	printf("\n");
-	
-	deleteion();
-	insert(d);
-	
-	viewlist();
-	printf("\n");
-	
-	insert(c);
-	insert(d);
-	viewlist();
-	printf("\n");
-	
-	deleteion();
-		viewlist();
-	printf("\n");
-	deleteion();
-	
-		viewlist();
-	printf("\n");
-	deleteion();
-		viewlist();
-	printf("\n");
-	deleteion();
-		viewlist();
-	printf("\n");
-	
-		deleteion();
-
-	
+	deletion();
+	deletion();
+	deletion();
 }
